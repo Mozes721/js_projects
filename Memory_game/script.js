bda721f5d0585d50a022c60bd0d50a62875eda39
Richard <MozesTheGreat@yahoo.com>
2020-11-23 09:08:23 +0200

    changed
    Added functionality

--- Memory_game/script.js
+++ Memory_game/script.js
@@ -1,8 +1,33 @@
 
+var img_box = document.querySelectorAll("front face")
+
+
+var images = new Array();
+
+images[0].src = '/Memory_game/apple.jpg';
+images[1].src = 'Memory_game/banana.jpg';
+images[2].src = 'Memory_game/orange.jpg';
+images[3].src = '/Memory_game/apple.jpg';
+images[4].src = 'Memory_game/banana.jpg';
+images[5].src = 'Memory_game/orange.jpg';
+
+// Randomize array in place using Durenfeld shuffle // //#endregion
+function shuffleArray(images){
+    for (var i = images.length - 1; i >0; i--) {
+        var j = Math.floor(Math.random() * (i +1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+for (i = 0; i < img_box.length; ++i) {
+    for (i = 0; i < images.length; i++) {
+    img_box[i].style.background.src = image[i];
+}}
+
 function onCardClicked(e) {
     const target = e.currentTarget;
-    console.log("It workes", target.className);
     target.className  = target.className.replace('color-hidden','').trim();
-    target.className('front-face') = 'red';
+    
 }
 
